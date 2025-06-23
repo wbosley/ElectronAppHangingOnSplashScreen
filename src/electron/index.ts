92bec0291e7404e3515276424b1b358e3593269a
Will Bosley <will.bosley555@gmail.com>
2025-06-23 11:04:44 +0100

    Check for did-finish-load event

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -66,6 +66,14 @@ app.on("ready", async (): Promise<void> => {
     });
   }
 
+  window.webContents.on("did-finish-load", () => {
+    log("Main window did-finish-load");
+    // if (!window.isVisible()) {
+    //   window.show();
+    //   log("Main window shown after did-finish-load");
+    // }
+  });
+
   window.once("ready-to-show", () => {
     log("Main window ready-to-show, showing main window");
     window.show();
